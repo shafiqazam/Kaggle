7e01f1e0a6d508f5a4652f31f0e2973ac020ea52
Shafiq <shafiq.azam@alumni.fh-aachen.de>
2025-12-01 21:12:55 +0800

    spaceship competition

---
+++ Machine Failures/test.py
@@ -0,0 +1,5 @@
+import os
+os.environ['KAGGLE_USERNAME'] = 'Shafiq Azam'
+os.environ['KAGGLE_KEY'] = 'KGAT_b666cdedc63a72c5713f24bd648f5700'
+# Then import kaggle or use the API
+from kaggle import api
